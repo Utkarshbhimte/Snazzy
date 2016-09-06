4a44d96eb4780acd5ec0ff6d46909691abbeb2cc
UtkarshBhimte <utkarshbhimte95@gmail.com>
2016-09-06 22:56:21 +0530

    First Commit

---
+++ js/script.js
@@ -0,0 +1,43 @@
+var searchTerm = '';
+
+$(document).ready(function() {
+    /*
+    $( ' #search-bar ' ).on('focus',function(){
+      $( '.search-wrap' ).addClass('active');
+      $( '#card' ).addClass('active');
+    });
+
+    */
+    document.getElementById('search-bar').onkeypress = function(e) {
+        var event = e || window.event;
+        var charCode = event.which || event.keyCode;
+        if (charCode == '13') {
+            // Enter pressed
+            searchTerm = $('#search-bar').val().toUpperCase();
+            console.log('user asked for "' + searchTerm + '"');
+            fetchMovieData();
+            return false;
+        }
+    }
+
+});
+
+function fetchMovieData() {
+    console.log('Fetching MetaData. . . ');
+    $.getJSON("http://www.omdbapi.com/?t="+searchTerm+"&y=&plot=short&r=json", function(movie) {
+        console.log(movie);
+      console.log('Fetched');
+      $( '.search-wrap' ).addClass('active');
+      $( '#card' ).addClass('active');
+      $( '#movieTitle' ).html(movie.Title);
+      $( '#movieYear' ).html(movie.Year);
+      $( '#movieGenre' ).html(movie.Genre);
+      $( '#movieRunTime' ).html(movie.Runtime);
+      $( '#moviePlot' ).html(movie.Plot);
+      $( '#movieDirector' ).html(movie.Director);
+      $( '#movieCast' ).html(movie.Actors);
+      $( '#poster' ).css('background-image', 'url('+ movie.Poster+')');
+      console.log(movie.Poster);
+      console.log(movie.Title);
+    });
+}
